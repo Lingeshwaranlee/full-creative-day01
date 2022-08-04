fcdee125b482a46dec7cf2844b1dd0ac4c52f826
lingeshwaran <94908980+Lingeshwaranlee@users.noreply.github.com>
2022-08-04 11:58:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/* 1) using varible & console.log: 
+     var lee='hello world';
+      console.log(lee);
+*/
+
+ /* 2)using document.write()
+  document.write("<h1>hello world</h1>"); */
+
+ /*3) using queryselector()
+ let name=document.querySelector("h1");
+name.innerText="hello world"
+ */
+/*4) using getElementById()
+ let name=document.getElementById("heading");
+name.innerText="hello world"
+ */
+ /*5) using getElementsByClassName()
+ let name1=document.getElementsByClassName("heading1");
+name1.innerHTML="hello world"
+ */
